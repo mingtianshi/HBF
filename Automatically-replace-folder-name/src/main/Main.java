b34d8a017a4b876f0349d8b42cd7b952bf45f5fa
PaiGu <74260755+mingtianshi@users.noreply.github.com>
2021-07-26 18:26:03 +0800

    Not ready for the time being, to ensure that the file is deleted and uploaded to github

---
+++ Automatically-replace-folder-name/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Main{
+
+    public Main(){
+
+    }
+
+    /**
+     *
+     */
+    public void Util(){
+
+    }
+
+    /**
+     *
+     * Automatically-replace-folder-name
+     * @auother PaiGu
+     */
+    public static void main(String[] args) {
+        // 可视化
+        // 主要目的
+        new Main();
+    }
+}
